ad775ef322266f540e806308006788023d317211
slont <slont.maytry@gmail.com>
2018-05-26 16:45:27 +0900

    Theme storeモジュール作成

---
+++ store/modules/theme.js
@@ -0,0 +1,61 @@
+import qs from 'qs'
+import {baseState, baseActions, baseMutations} from './base'
+
+export const state = () => ({
+  ...baseState
+})
+
+export const mutations = {
+  init(state) {
+    baseMutations.init(state, `/themes`)
+  }
+}
+
+export const actions = {
+  ...baseActions,
+
+  findByTagName({state}, params = {query: {}}) {
+    return this.$axios({
+      url: `${state.endpoint}/_tag?${qs.stringify(params.query, {indices: false})}`,
+      method: 'GET',
+      mode: 'cors',
+      credentials: 'include'
+    })
+  },
+
+  findByNew({state}, params = {query: {}}) {
+    return this.$axios({
+      url: `${state.endpoint}/_new?${qs.stringify(params.query, {indices: false})}`,
+      method: 'GET',
+      mode: 'cors',
+      credentials: 'include'
+    })
+  },
+
+  findOneFavorite({state}, params = {themeId: '', userId: ''}) {
+    return this.$axios({
+      url:`${state.endpoint}/${params.themeId}/favorites/${params.userId}`,
+      method: 'GET',
+      mode: 'cors',
+      credentials: 'include'
+    })
+  },
+
+  updateFavorite({state}, params = {themeId: '', userId: ''}) {
+    return this.$axios({
+      url: `${state.endpoint}/${params.themeId}/favorites/${params.userId}`,
+      method: 'PUT',
+      mode: 'cors',
+      credentials: 'include'
+    })
+  },
+
+  deleteFavorite({state}, params = {themeId: '', userId: ''}) {
+    return state.$axios({
+      url: `${this.endpoint}/${params.themeId}/favorites/${params.userId}`,
+      method: 'DELETE',
+      mode: 'cors',
+      credentials: 'include'
+    })
+  }
+}
